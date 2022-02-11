5856d65d9d22ea653375c08452d85583718c845e
Srajan R <srrjanr@gmail.com>
2022-02-11 10:31:48 +0530

    Initial commit

---
+++ base.h
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+void clear() {
+    system("cls");
+}
---
+++ introC_c1.cpp
@@ -0,0 +1,45 @@
+#include "base.h"
+
+int main() {
+    clear();
+    
+    // cout << "Hello World\n";
+
+    // int N;
+    // cin >> N;
+    // int x = N + 2;
+    // for (int i = 0; i < N + 1; i++) {
+    //     cout << "Val of i: " << i << endl;
+    //     cout << "Val of x * i: " << x * i << endl;
+    //     x = x + i * N;
+    //     cout << "Val of updated x: " << x << endl;
+    // }
+    
+    // int N;
+    // cin >> N;
+    // for (int i = 0; i < N + 1; i++)
+    // {
+    //     cout << "Value of i: " << i  << "\t";
+    //     cout << N * N - 2 * i << endl;
+    // }
+    
+    // int x = 7, y = 9, len = 7, wid = 8;
+    // for (int i = 0; i < y; i++) {
+    //     cout << "\n";
+    // }
+    // for (int j = 0; j < wid; j++) {
+    //     for (int i = 0; i < x; i++) {
+    //         cout << " ";
+    //     }
+    //     cout << "*";
+    //     if (j == 0 || j == wid - 1)
+    //         for (int i = 0; i < len - 2; i++) cout << "*";
+    //     else
+    //         for (int i = 0; i < len - 2; i++) cout << " ";
+    //     cout << "*" << endl;
+    // }
+    
+
+
+    return 0;
+}
