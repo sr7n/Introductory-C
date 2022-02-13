667b2a11740fc7275c71fb94b8f1bcd76155e10b
Srajan R <srrjanr@gmail.com>
2022-02-13 11:13:18 +0530

    13 Feb

--- Code/base.h
+++ Code/base.h
@@ -21,3 +21,33 @@ void clear() {
 
 int random[10] = {45, 64, 24, 6, 4, 35, 76, 87, 66, 0};
 Point randoms[5] = {{45, 64}, {24, 13}, {44, 35}, {76, 87}, {66, 78}};
+
+int anotherFunction(int a, int b) {
+  int answer = 42;
+  int x = 0;
+  printf("In anotherFunction(%d,%d)\n",a,b);
+  while (b > a) {
+    printf("a is %d, b is %d\n", a, b);
+    answer = answer + (b - a);
+    b -= x;
+    a += x / 2;
+    x++;
+  }
+  return answer;
+}
+
+int someFunction(int x, int y) {
+  int a = x + y;
+  if (x < y) {
+    for (int i = 0; i < x; i++) {
+      printf("In the loop with i = %d, a = %d\n", i, a);
+      a = a + x;
+      }
+  }
+  else {
+    y = anotherFunction(y,a+4);
+  }
+  return a * y;
+}
+
+enum Results {Fail = -1, Average, Good, Excellent, Outstanding};
--- Code/introC_c1.cpp
+++ Code/introC_c1.cpp
@@ -2,7 +2,7 @@
 
 int main() {
     clear();
-    
+
     // cout << "Hello World\n";
 
     // int N;
@@ -14,7 +14,7 @@ int main() {
     //     x = x + i * N;
     //     cout << "Val of updated x: " << x << endl;
     // }
-    
+
     // int N;
     // cin >> N;
     // for (int i = 0; i < N + 1; i++)
@@ -22,7 +22,7 @@ int main() {
     //     cout << "Value of i: " << i  << "\t";
     //     cout << N * N - 2 * i << endl;
     // }
-    
+
     // int x = 7, y = 9, len = 7, wid = 8;
     // for (int i = 0; i < y; i++) {
     //     cout << "\n";
@@ -38,7 +38,7 @@ int main() {
     //         for (int i = 0; i < len - 2; i++) cout << " ";
     //     cout << "*" << endl;
     // }
-    
+
     // Point my_point = {0, 0};
     // int closest = 26955;
     // for (int i = 0; i < 5; i++) {
@@ -49,12 +49,77 @@ int main() {
     //     }
     //     cout << i << "\t" << d << endl;
     // }
-    
+
     // int N = 6;
     // for (int i = 0; i <= (N + 1) * 2; i++) {
     //     cout << N*3 + i*2 << " ";
     // }
+
+    // int N = 8;
+    // switch (N) {
+    // case 1:
+    //     cout << "Helelo"; break;
+
+    // case 4:
+    //     cout << "HelHe,lelo"; break;
+
+    // case 8:
+    //     cout << "Helejjjjjjjjlo"; break;
+
+    // default:
+    //     cout << "Error"; break;
+    // }
+
+    // int x = 2, n = 3;
+    // for (int i = 0; i < n; i++) {
+    //     if (i % 2 == 0) {
+    //         x *= i + 1;
+    //         continue;
+    //     }
+    //     x--;
+    //     if (x == 0) {
+    //         break;
+    //     }
+    // }
+    // cout << x << "\t" << n;
+
+    // int n = 7;
+    // int ans = 0;
+    // for (int i = 1; i < n; i++) {
+    //     if (i < n / 2) {
+    //         ans -= i;
+    //     }
+    //     else {
+    //         ans += i;
+    //     }
+    // }
+    // cout << ans;
+
+
+//   int a = 3;
+//     int b = 6;
+//   while (a <= b) {
+//     if (a % 2 == 1) {
+//         printf("a is %d\n", a);
+//     }
+//     else {
+//       printf("b is %d\n", b);
+//       for (int i = 0; i < b - a ; i++) {
+//             printf("a * i + b = %d\n", a * i + b);
+//         }
+//     }
+//     a++;
+//     b--;
+//     }
+    // anotherFunction(2,9);
+    // int x = 2;
+    // int a = someFunction(x,3);
+    // printf("a = %d\n", a);
+    // printf("x = %d\n", x);
+
+    cout << Results::Excellent;
     
+
 
     return 0;
 }
